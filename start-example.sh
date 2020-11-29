3ed213d8734ede12ebfe1f183e944bfd39327191
Tyler Lowrey <contact@tylerlowrey.com>
2020-11-29 05:03:03 +0000

    Added start script

---
+++ start-example.sh
@@ -0,0 +1,6 @@
+export MODERATION_BOT_DATABASE_HOST=""
+export MODERATION_BOT_DATABASE_NAME=""
+export MODERATION_BOT_DATABASE_USERNAME=""
+export MODERATION_BOT_DATABASE_PASSWORD=""
+
+dotnet run --config Release --project ModerationBot
